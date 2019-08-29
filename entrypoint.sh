25aa1167b0a02615bef41bcd68aac468e8617170
apiloqbc <48203828+apiloqbc@users.noreply.github.com>
2019-08-29 16:53:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,8 @@
 
 set -e
 
-# skip if no /revert
-echo "Checking if contains '/revert' command..."
-(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -E "/revert") || exit 78
-
-# skip if not a PR
-echo "Checking if a PR command..."
-(jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
-
-# get the SHA to revert
-COMMIT_TO_REVERT=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | cut -c 9-)
-
-if [[ "$(jq -r ".action" "$GITHUB_EVENT_PATH")" != "created" ]]; then
-	echo "This is not a new comment event!"
-	exit 78
-fi
-
-PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-echo "Collecting information about PR #$PR_NUMBER of $REPO_FULLNAME..."
+echo "Registering the ip"
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "Set the GITHUB_TOKEN env variable."
@@ -30,9 +13,6 @@ fi
 URI=https://api.github.com
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
-
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$REPO_FULLNAME/pulls/$PR_NUMBER")
 
 HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
 HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
@@ -46,9 +26,7 @@ set -o xtrace
 git fetch origin $HEAD_BRANCH
 
 # do the revert
-git checkout -b $HEAD_BRANCH origin/$HEAD_BRANCH
 
-# check commit exists
-git cat-file -t $COMMIT_TO_REVERT
-git revert $COMMIT_TO_REVERT --no-edit
+date >> README.md
+
 git push origin $HEAD_BRANCH
