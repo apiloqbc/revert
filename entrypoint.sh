f84985bb8d82b4a2ae63618764c11194a70e1044
apiloqbc <48203828+apiloqbc@users.noreply.github.com>
2019-08-29 21:02:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
 HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-git config --global user.email "revert@github.com"
-git config --global user.name "GitHub Revert Action"
+git config --global user.email $EMAIL
+git config --global user.name $GHUSER
 
 set -o xtrace
 
